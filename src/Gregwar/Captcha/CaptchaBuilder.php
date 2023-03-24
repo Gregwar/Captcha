6e5b61b66ac89885b505153f4ef9a74ffa5b3074
Grégoire Passault <g.passault@gmail.com>
2023-03-24 23:12:41 +0100

    Removing useless un-declared variables

--- src/Gregwar/Captcha/CaptchaBuilder.php
+++ src/Gregwar/Captcha/CaptchaBuilder.php
@@ -427,7 +427,6 @@ class CaptchaBuilder implements CaptchaBuilderInterface
                 $color = $this->backgroundColor;
                 $bg = imagecolorallocate($image, $color[0], $color[1], $color[2]);
             }
-            $this->background = $bg;
             imagefill($image, 0, 0, $bg);
         } else {
             // use a random background image
