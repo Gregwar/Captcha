0e4f57bb22a3d8b8437c551d0c7c25251b76e066
João Pedro <joao_pedro01@terra.com.br>
2025-01-28 18:35:41 +0000

    fix[font]: removed font style that forced uppercase characters

--- src/Gregwar/Captcha/CaptchaBuilder.php
+++ src/Gregwar/Captcha/CaptchaBuilder.php
@@ -415,7 +415,7 @@ class CaptchaBuilder implements CaptchaBuilderInterface
         }
 
         if ($font === null) {
-            $font = __DIR__ . '/Font/captcha'.$this->rand(0, 5).'.ttf';
+            $font = __DIR__ . '/Font/captcha'.$this->rand(0, 4).'.ttf';
         }
 
         if (empty($this->backgroundImages)) {
