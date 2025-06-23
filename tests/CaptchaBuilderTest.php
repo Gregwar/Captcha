9607feecde6bf9ea7b6555d39fabd9c5490af520
Kieran <kieran.brahney@gmail.com>
2025-06-23 11:19:17 +0100

    Add tests (#140)
    Co-authored-by: Roman Kelesidis <roman25052006.kelesh@gmail.com>

--- tests/CaptchaBuilderTest.php
+++ tests/CaptchaBuilderTest.php
@@ -7,6 +7,34 @@ use PHPUnit\Framework\TestCase;
 
 class CaptchaBuilderTest extends TestCase
 {
+    /**
+     * Captcha phrases
+     *
+     * @var array
+     */
+    private $phrases = array(
+        '@!#*?',
+        's3cr3t',
+        'p4ssw0rd',
+        'hello',
+        'world'
+    );
+
+    public function testCreate()
+    {
+        $this->assertInstanceOf('Gregwar\Captcha\CaptchaBuilder', CaptchaBuilder::create());
+    }
+
+    public function testBuild()
+    {
+        $this->assertInstanceOf('Gregwar\Captcha\CaptchaBuilder', CaptchaBuilder::create()->build());
+
+        foreach ($this->phrases as $phrase) {
+            $builder = new CaptchaBuilder($phrase);
+            $this->assertEquals($phrase, $builder->getPhrase());
+        }
+    }
+
     public function testDemo()
     {
         $captcha = new CaptchaBuilder();
