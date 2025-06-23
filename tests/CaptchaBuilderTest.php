c297a7732bc3a934cad7a8af463686dc804078ca
Kieran <kieran.brahney@gmail.com>
2025-06-23 11:55:53 +0100

    Ability to change image output type (#141)

--- tests/CaptchaBuilderTest.php
+++ tests/CaptchaBuilderTest.php
@@ -55,4 +55,37 @@ class CaptchaBuilderTest extends TestCase
 
         $this->assertTrue(is_int($int));
     }
+
+    public function testImageType()
+    {
+        foreach (array('jpeg' => IMAGETYPE_JPEG, 'png' => IMAGETYPE_PNG, 'gif' => IMAGETYPE_GIF) as $type => $expected) {
+            $captcha = new CaptchaBuilder();
+            $captcha->setImageType($type)->build();
+
+            // Test save()
+            $captcha->save('out.'.$type);
+            $this->assertType(__DIR__ . '/../out.' . $type, $expected);
+
+            // Test output()
+            ob_start();
+            $captcha->output();
+            file_put_contents(__DIR__ . '/../out.' . $type, ob_get_clean());
+            $this->assertType(__DIR__ . '/../out.' . $type, $expected);
+        }
+    }
+
+    /**
+     * @param string $file
+     * @param int $expected IMAGETYPE_JPEG / IMAGETYPE_PNG / IMAGETYPE_GIF
+     * @return void
+     */
+    private function assertType($file, $expected)
+    {
+        $info = getimagesize($file);
+        if ($info === false) {
+            $this->fail("Not a valid image.");
+        } else {
+            $this->assertSame($expected, $info[2], 'Unexpected image type.');
+        }
+    }
 }
