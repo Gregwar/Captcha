22c4f6a0fd277c800612a86275c6e9255d988c00
lachlan <lachlan.00@gmail.com>
2026-06-04 19:44:55 +1000

    copilot request comments

--- src/Gregwar/Captcha/CaptchaBuilderInterface.php
+++ src/Gregwar/Captcha/CaptchaBuilderInterface.php
@@ -11,20 +11,20 @@ interface CaptchaBuilderInterface
      * Builds the code
      * @param int[] $fingerprint
      */
-    public function build(int $width, int $height, ?string $font, ?array $fingerprint): static;
+    public function build(int $width = 150, int $height = 40, ?string $font = null, ?array $fingerprint = null): static;
 
     /**
      * Saves the code to a file
      */
-    public function save(string $filename, int $quality): void;
+    public function save(?string $filename = null, int $quality = 90): void;
 
     /**
      * Gets the image contents
      */
-    public function get(int $quality): string;
+    public function get(int $quality = 90): string;
 
     /**
      * Outputs the image
      */
-    public function output(int $quality): void;
+    public function output(int $quality = 90): void;
 }
